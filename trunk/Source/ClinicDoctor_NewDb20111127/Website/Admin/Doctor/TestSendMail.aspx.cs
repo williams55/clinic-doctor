ab0529f38468e6e9633a9bdb757d8cf6d2f61523
votienphat@gmail.com <votienphat@gmail.com@81d2c78d-55ee-6d77-9ada-d03d0910548c>
2011-12-08 04:03:27 +0000

    Test send calendar via email

--- trunk/Source/ClinicDoctor_NewDb20111127/Website/Admin/Doctor/TestSendMail.aspx.cs
+++ trunk/Source/ClinicDoctor_NewDb20111127/Website/Admin/Doctor/TestSendMail.aspx.cs
@@ -14,28 +14,28 @@ public partial class Admin_Doctor_TestSendMail : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        DateTime startTime = DateTime.Now.AddHours(1);
-        DateTime endTime = startTime.AddHours(1);
-
         SmtpClient sc = new SmtpClient("smtp.gmail.com");
         sc.Port = 587;
-        sc.Credentials = new System.Net.NetworkCredential("votienphat", "@@@chilaquakhu");
+        sc.Credentials = new System.Net.NetworkCredential("username", "password");
         sc.EnableSsl = true;
 
         MailMessage msg = new MailMessage();
         msg.From = new MailAddress("votienphat@gmail.com", "Đại bàng Dép Đứt");
-        msg.To.Add(new MailAddress("quangvy@gmail.com@gmail.com", "Chim én Heroes :))"));
-        msg.Subject = "Thử nghiệm gửi appointment nà";
-        msg.Body = "Ê, thấy tao gửi zậy được chưa ku tèo";
+        msg.To.Add(new MailAddress(" vy.le@internationalsos.com", "Chim én Heroes :))"));
+        msg.Subject = "Sát thủ đầu mưng mủ";
+        msg.Body = "Một khi đã máu thì đừng hỏi bố cháu là ai :))";
 
         iCalendar iCal = new iCalendar();
 
         iCal.Method = "PUBLISH";
 
+        DateTime startTime = DateTime.Now.AddHours(3);
+        DateTime endTime = startTime.AddHours(1);
+
         // Create the event
         Event evt = iCal.Create<Event>();
 
-        evt.Summary = "Tóm tắt về Appointment nè. Tiêu đề ở đây.";
+        evt.Summary = "Một khi đã quyết chỉ có nản mới cản được anh.";
         evt.Start = new iCalDateTime(startTime.Year, startTime.Month, startTime.Day, startTime.Hour, startTime.Minute, startTime.Second);
         evt.End = evt.Start.AddMinutes(30);
         evt.Description = "Chi tiết appointment";
