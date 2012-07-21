787ad206af22d5c8523192d699a8ec34318176f1
pminhtuan89@gmail.com <pminhtuan89@gmail.com@81d2c78d-55ee-6d77-9ada-d03d0910548c>
2012-07-21 15:59:24 +0000

    update room

--- trunk/Source/AppointmentSystem/ApptSite/Admin/Room/Default.aspx.cs
+++ trunk/Source/AppointmentSystem/ApptSite/Admin/Room/Default.aspx.cs
@@ -11,35 +11,111 @@ using AppointmentSystem.Settings.BusinessLayer;
 using DevExpress.Web.ASPxGridView;
 using DevExpress.Web.Data;
 using DevExpress.Web.ASPxEditors;
+using AppointmentBusiness.Util;
+using Appt.Common.Constants;
+using Log.Controller;
+using Common.Util;
 
 public partial class Admin_Room_edit : System.Web.UI.Page
 {
+    string ScreenCode = "Room";
+    static string _message;
     protected void Page_Load(object sender, EventArgs e)
     {
+        try
+        {
+            if (!RightAccess.CheckUserRight(EntitiesUtilities.GetAuthName(), ScreenCode, OperationConstant.Read.Key, out _message))
+            {
+                WebCommon.ShowDialog(this, _message, WebCommon.GetHomepageUrl(this));
+                gridRoom.Visible = false;
+                return;
+            }
+            var gridcolums = gridRoom.Columns["btnCommand"] as GridViewCommandColumn;
+            if (gridcolums == null) return;
+            gridcolums.EditButton.Visible = RightAccess.CheckUserRight(EntitiesUtilities.GetAuthName(), ScreenCode, OperationConstant.Update.Key, out _message);
+            gridcolums.NewButton.Visible = RightAccess.CheckUserRight(EntitiesUtilities.GetAuthName(), ScreenCode, OperationConstant.Create.Key, out _message);
+            var btnDelete = gridcolums.CustomButtons.Find(x => x.ID == "btnDelete");
+            if (!RightAccess.CheckUserRight(EntitiesUtilities.GetAuthName(), ScreenCode, OperationConstant.Delete.Key, out _message))
+            {
+                btnDelete.Visibility = GridViewCustomButtonVisibility.Invisible;
+            }
 
+            gridcolums.Visible = gridcolums.EditButton.Visible || gridcolums.NewButton.Visible || (btnDelete.Visibility != GridViewCustomButtonVisibility.Invisible);
+        }
+        catch (Exception ex)
+        {
+            LogController.WriteLog(System.Runtime.InteropServices.Marshal.GetExceptionCode(), ex, Network.GetIpClient());
+        }
     }
     protected void gridRoom_CustomButtonCallback(object sender, DevExpress.Web.ASPxGridView.ASPxGridViewCustomButtonCallbackEventArgs e)
     {
-        if (e.ButtonID != "btnDelete") return;
-        Int64 id;
-        if (Int64.TryParse(gridRoom.GetRowValues(e.VisibleIndex, "Id").ToString(), out id))
+        try
         {
-
-            var obj = DataRepository.RoomProvider.GetById(int.Parse(id.ToString()));
-            obj.IsDisabled = true;
-            obj.UpdateUser = WebCommon.GetAuthUsername();
-            obj.UpdateDate = DateTime.Now;
-            DataRepository.RoomProvider.Update(obj);
+            if (!RightAccess.CheckUserRight(EntitiesUtilities.GetAuthName(), ScreenCode, OperationConstant.Delete.Key, out _message))
+            {
+                WebCommon.ShowDialog(this, _message, WebCommon.GetHomepageUrl(this));
+                return;  
+            }
+            if (e.ButtonID != "btnDelete") return;
+            Int64 id;
+            if (Int64.TryParse(gridRoom.GetRowValues(e.VisibleIndex, "Id").ToString(), out id))
+            {
+                var obj = DataRepository.RoomProvider.GetById(int.Parse(id.ToString()));
+                if (obj == null||obj.IsDisabled)
+                {
+                    ((ASPxGridView)sender).JSProperties[GeneralConstants.ApptMessage] = "Service is not existed. You cannot delete it.";
+                    return;
+                }
+                DataRepository.RoomProvider.DeepLoad(obj);
+                if (obj.RosterCollection.Exists(x => !x.IsDisabled) || obj.DoctorRoomCollection.Exists(x => !x.IsDisabled) )
+                {
+                    ((ASPxGridView)sender).JSProperties[GeneralConstants.ApptMessage] = String.Format("Service {0} is using, you cannot delete it.", obj.Title);
+                    return;
+                }
+                obj.IsDisabled = true;
+                obj.UpdateUser = WebCommon.GetAuthUsername();
+                obj.UpdateDate = DateTime.Now;
+                DataRepository.RoomProvider.Update(obj);
+            }
+        }
+        catch( Exception ex)
+        {
+            LogController.WriteLog(System.Runtime.InteropServices.Marshal.GetExceptionCode(), ex, Network.GetIpClient());
         }
     }
     protected void gridRoom_RowInserting(object sender, DevExpress.Web.Data.ASPxDataInsertingEventArgs e)
     {
-        e.NewValues["CreateUser"] = e.NewValues["UpdateUser"] = WebCommon.GetAuthUsername();
-        e.NewValues["CreateDate"] = e.NewValues["UpdateDate"] = DateTime.Now;
+        try {
+            if (!RightAccess.CheckUserRight(EntitiesUtilities.GetAuthName(), ScreenCode, OperationConstant.Create.Key, out _message))
+            {
+                WebCommon.AlertGridView(sender, _message);
+                e.Cancel = true;
+                return;
+            }
+            e.NewValues["CreateUser"] = e.NewValues["UpdateUser"] = WebCommon.GetAuthUsername();
+            e.NewValues["CreateDate"] = e.NewValues["UpdateDate"] = DateTime.Now;
+        }
+        catch( Exception ex)
+        {
+            LogController.WriteLog(System.Runtime.InteropServices.Marshal.GetExceptionCode(), ex, Network.GetIpClient());
+        }
     }
     protected void gridRoom_RowUpdating(object sender, DevExpress.Web.Data.ASPxDataUpdatingEventArgs e)
     {
-        e.NewValues["CreateUser"] = e.NewValues["UpdateUser"] = WebCommon.GetAuthUsername();
-        e.NewValues["CreateDate"] = e.NewValues["UpdateDate"] = DateTime.Now;
+        try
+        {
+            if (!RightAccess.CheckUserRight(EntitiesUtilities.GetAuthName(), ScreenCode, OperationConstant.Update.Key, out _message))//Kiem tra xem user co quyen cap nhat hay khong
+            {
+                WebCommon.AlertGridView(sender, _message);
+                e.Cancel = true;
+                return;
+            }
+            e.NewValues["CreateUser"] = e.NewValues["UpdateUser"] = WebCommon.GetAuthUsername();
+            e.NewValues["CreateDate"] = e.NewValues["UpdateDate"] = DateTime.Now;
+        }
+        catch (Exception ex)
+        {
+            LogController.WriteLog(System.Runtime.InteropServices.Marshal.GetExceptionCode(), ex, Network.GetIpClient());
+        }
     }
 }
