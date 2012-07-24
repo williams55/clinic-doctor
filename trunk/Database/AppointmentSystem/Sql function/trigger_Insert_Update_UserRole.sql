83ce30491f0121230fa8a086e1af75081d9a21e2
pminhtuan89@gmail.com <pminhtuan89@gmail.com@81d2c78d-55ee-6d77-9ada-d03d0910548c>
2012-07-24 14:07:21 +0000

    add trigger  insert role for user when insert,update user

---
+++ trunk/Database/AppointmentSystem/Sql function/trigger_Insert_Update_UserRole.sql
@@ -0,0 +1,29 @@
+--trigger add role for user when create user
+alter trigger add_rolegroup_user 
+on Users after Insert,Update
+as
+	SET NOCOUNT ON
+	declare @UserId nvarchar(50),
+			@GroupUserId nvarchar(20),
+			@CreateUser nvarchar(200),
+			@CreateDate datetime,
+			@RoleId int
+	select @UserId=i.Id,@GroupUserId=i.UserGroupId,@CreateUser=i.CreateUser,@CreateDate=i.CreateDate from Inserted i
+	print(@UserId)
+	declare cGroupRole cursor for
+			select RoleId from GroupRole where GroupId=@GroupUserId and IsDisabled='false'
+	open cGroupRole
+	fetch next from cGroupRole into @RoleId
+	while @@fetch_status=0
+		begin
+			if not exists(select Id from UserRole ur where ur.UserId=@UserId and ur.RoleId=@RoleId and IsDisabled='false' )
+				begin
+					
+					insert into UserRole(UserId,RoleId,IsDisabled,CreateUser,CreateDate,UpdateUser,UpdateDate)
+							values(@UserId,@RoleId,'false',@CreateUser,@CreateDate,@CreateUser,@CreateDate)				
+				end
+				fetch next from cGroupRole into @RoleId
+		end
+	close cGroupRole
+	Deallocate cGroupRole
+go
