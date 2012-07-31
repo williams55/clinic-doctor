6a789501206fa07b76ac77be41266c756329b337
votienphat <votienphat@gmail.com>
2012-07-31 15:34:35 +0000

    Fixed analog clock in appointment page

--- Source/AppointmentSystem/ApptSite/resources/components/analogClock/analogclock.js
+++ Source/AppointmentSystem/ApptSite/resources/components/analogClock/analogclock.js
@@ -1,11 +1,39 @@
-$(document).ready(function() {
+function GenCss(value, width, height) {
+    var matrix = CSS3Helpers.getTransformationMatrix(value, true);
+    
+    var filterCode = config.getScriptedValue('transformToMatrix.templates.ieFilters',
+				{
+				    M11: matrix.e(1, 1),
+				    M12: matrix.e(1, 2),
+				    M21: matrix.e(2, 1),
+				    M22: matrix.e(2, 2),
+				    width: width + "px",
+				    height: height + "px",
+				    transform: value.trim().replace(/\s+/g, ' ').replace(/\)(\s)/g, ')<br>                         ')
+				});
+    console.log(filterCode);
+}
 
+$(document).ready(function() {
     setInterval(function() {
         var seconds = new Date().getSeconds();
         var sdegree = seconds * 6;
         var srotate = "rotate(" + sdegree + "deg)";
 
-        $("#sec").css({ "-moz-transform": srotate, "-webkit-transform": srotate });
+        var matrix = CSS3Helpers.getTransformationMatrix(srotate, true);
+
+        $("#sec").css({
+            "-moz-transform": srotate,
+            "-webkit-transform": srotate,
+            "-ms-transform": srotate,
+            "transform": srotate,
+            "-o-transform": srotate,
+            "filter": "progid:DXImageTransform.Microsoft.Matrix(M11=" + matrix.e(1, 1)
+            + ", M12=" + matrix.e(1, 2)
+            + ", M21=" + matrix.e(2, 1)
+            + ", M22=" + matrix.e(2, 2)
+            + ")"
+        });
 
     }, 1000);
 
@@ -16,7 +44,20 @@ $(document).ready(function() {
         var hdegree = hours * 30 + (mins / 2);
         var hrotate = "rotate(" + hdegree + "deg)";
 
-        $("#hour").css({ "-moz-transform": hrotate, "-webkit-transform": hrotate });
+        var matrix = CSS3Helpers.getTransformationMatrix(hrotate, true);
+
+        $("#hour").css({
+            "-moz-transform": hrotate,
+            "-webkit-transform": hrotate,
+            "-ms-transform": hrotate,
+            "transform": hrotate,
+            "-o-transform": hrotate,
+            "filter": "progid:DXImageTransform.Microsoft.Matrix(M11=" + matrix.e(1, 1)
+            + ", M12=" + matrix.e(1, 2)
+            + ", M21=" + matrix.e(2, 1)
+            + ", M22=" + matrix.e(2, 2)
+            + ")"
+        });
 
     }, 1000);
 
@@ -26,7 +67,20 @@ $(document).ready(function() {
         var mdegree = mins * 6;
         var mrotate = "rotate(" + mdegree + "deg)";
 
-        $("#min").css({ "-moz-transform": mrotate, "-webkit-transform": mrotate });
+        var matrix = CSS3Helpers.getTransformationMatrix(mrotate, true);
+
+        $("#min").css({
+            "-moz-transform": mrotate,
+            "-webkit-transform": mrotate,
+            "-ms-transform": mrotate,
+            "transform": mrotate,
+            "-o-transform": mrotate,
+            "filter": "progid:DXImageTransform.Microsoft.Matrix(M11=" + matrix.e(1, 1)
+            + ", M12=" + matrix.e(1, 2)
+            + ", M21=" + matrix.e(2, 1)
+            + ", M22=" + matrix.e(2, 2)
+            + ")"
+        });
 
     }, 1000);
 
