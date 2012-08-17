c2f1a3b575f2088b0b10a9d26ab97ccb862a3856
votienphat <votienphat@gmail.com>
2012-08-17 07:37:14 +0000

    Update data Users, Roster, Appointment and Room

---
+++ Database/SVN/Data/dbo.Room_Data.sql
@@ -0,0 +1,5 @@
+SET IDENTITY_INSERT [dbo].[Room] ON
+INSERT INTO [dbo].[Room] ([Id], [Title], [Note], [ServicesId], [IsDisabled], [CreateUser], [CreateDate], [UpdateUser], [UpdateDate]) VALUES (3, N'Office', NULL, 8, 0, N'GOWU', '2012-08-16 20:59:05.327', N'GOWU', '2012-08-16 20:59:05.327')
+INSERT INTO [dbo].[Room] ([Id], [Title], [Note], [ServicesId], [IsDisabled], [CreateUser], [CreateDate], [UpdateUser], [UpdateDate]) VALUES (4, N'Xray 1', NULL, 9, 0, N'GOWU', '2012-08-16 20:59:16.050', N'GOWU', '2012-08-16 20:59:16.050')
+INSERT INTO [dbo].[Room] ([Id], [Title], [Note], [ServicesId], [IsDisabled], [CreateUser], [CreateDate], [UpdateUser], [UpdateDate]) VALUES (5, N'ENT 1', NULL, 8, 0, N'GOWU', '2012-08-16 20:59:25.873', N'GOWU', '2012-08-16 20:59:25.873')
+SET IDENTITY_INSERT [dbo].[Room] OFF
