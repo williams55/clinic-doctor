30a0020b9270cef43fab5dd6bf3879fe5961dc6d
quangvy <quangvy@gmail.com>
2012-09-17 08:22:35 +0000

    Add Demo Data

--- Database/SVN/Data/dbo.UserRole_Data.sql
+++ Database/SVN/Data/dbo.UserRole_Data.sql
@@ -1,3 +1,7 @@
 SET IDENTITY_INSERT [dbo].[UserRole] ON
+INSERT INTO [dbo].[UserRole] ([Id], [Username], [RoleId], [IsDisabled], [CreateUser], [CreateDate], [UpdateUser], [UpdateDate]) VALUES (4, N'vy.le', 1, 0, N'GOWU', '2012-06-18 13:51:03.957', N'GOWU', '2012-06-18 13:51:03.957')
+INSERT INTO [dbo].[UserRole] ([Id], [Username], [RoleId], [IsDisabled], [CreateUser], [CreateDate], [UpdateUser], [UpdateDate]) VALUES (5, N'duong.phan', 1, 0, N'GOWU', '2012-06-18 13:51:03.957', N'GOWU', '2012-06-18 13:51:03.957')
+SET IDENTITY_INSERT [dbo].[UserRole] OFF
+SET IDENTITY_INSERT [dbo].[UserRole] ON
 INSERT INTO [dbo].[UserRole] ([Id], [Username], [RoleId], [IsDisabled], [CreateUser], [CreateDate], [UpdateUser], [UpdateDate]) VALUES (3, N'GOWU', 1, 0, N'duong.phan', '2012-07-19 00:00:00.000', N'duong.phan', '2012-07-19 00:00:00.000')
 SET IDENTITY_INSERT [dbo].[UserRole] OFF
