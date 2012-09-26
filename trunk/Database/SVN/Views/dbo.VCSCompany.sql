6dc181a7677c81db44960341a04c08dbe6f2bec2
votienphat@gmail.com <votienphat@gmail.com@81d2c78d-55ee-6d77-9ada-d03d0910548c>
2012-09-26 16:13:27 +0000

    Add view from VCS

---
+++ trunk/Database/SVN/Views/dbo.VCSCompany.sql
@@ -0,0 +1,15 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+
+
+
+CREATE VIEW [dbo].[VCSCompany]
+AS
+SELECT     VCS.dbo.Company.*
+FROM         VCS.dbo.Company
+
+
+
+GO
---
+++ trunk/Database/SVN/Views/dbo.VCSMemberType.sql
@@ -0,0 +1,13 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+
+
+CREATE VIEW [dbo].[VCSMemberType]
+AS
+SELECT     VCS.dbo.MemberType.*
+FROM         VCS.dbo.MemberType
+
+
+GO
