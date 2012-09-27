692ae0e14f2948a8deede3cfa7b37b566ad07175
votienphat <votienphat@gmail.com>
2012-09-27 15:54:15 +0000

    Add SP select patient by patient code

---
+++ Database/SVN/Stored Procedures/dbo._VCSPatient_GetByPatientCode.sql
@@ -0,0 +1,26 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[_VCSPatient_GetByPatientCode]
+	-- Add the parameters for the stored procedure here
+	@PatientCode nchar(11)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT * FROM [VCS].[dbo].[Patient]
+			WHERE [PatientCode] = @PatientCode
+
+END
+
+GO
