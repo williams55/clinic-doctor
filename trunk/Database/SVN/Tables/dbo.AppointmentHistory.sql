9e9cbf940a4286c1e4af828aa57f4096410ef243
votienphat@gmail.com <votienphat@gmail.com@81d2c78d-55ee-6d77-9ada-d03d0910548c>
2012-10-03 08:40:58 +0000

    Them bang history cua appointment, update SP tao patient tra ve doi tuong sau khi tao xong

---
+++ trunk/Database/SVN/Tables/dbo.AppointmentHistory.sql
@@ -0,0 +1,11 @@
+CREATE TABLE [dbo].[AppointmentHistory]
+(
+[Guid] [uniqueidentifier] NOT NULL,
+[AppointmentId] [nvarchar] (20) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[Note] [nvarchar] (500) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[CreateUser] [nvarchar] (200) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[CreateDate] [datetime] NOT NULL CONSTRAINT [DF_AppointmentHistory_CreateDate] DEFAULT (getdate())
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[AppointmentHistory] ADD CONSTRAINT [PK_AppointmentHistory] PRIMARY KEY CLUSTERED  ([Guid]) ON [PRIMARY]
+GO
