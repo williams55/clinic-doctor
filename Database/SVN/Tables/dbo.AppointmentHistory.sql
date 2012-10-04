46dd57e912e18f03a68787c5d33f8f2abff313ed
votienphat <votienphat@gmail.com>
2012-10-04 15:27:32 +0000

    Doi kieu du lieu cua PatientCode trong Appointment tu nchar10 sang nchar11

--- Database/SVN/Tables/dbo.AppointmentHistory.sql
+++ Database/SVN/Tables/dbo.AppointmentHistory.sql
@@ -6,6 +6,8 @@ CREATE TABLE [dbo].[AppointmentHistory]
 [CreateUser] [nvarchar] (200) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [CreateDate] [datetime] NOT NULL CONSTRAINT [DF_AppointmentHistory_CreateDate] DEFAULT (getdate())
 ) ON [PRIMARY]
+ALTER TABLE [dbo].[AppointmentHistory] ADD
+CONSTRAINT [FK_AppointmentHistory_Appointment] FOREIGN KEY ([AppointmentId]) REFERENCES [dbo].[Appointment] ([Id])
 GO
 ALTER TABLE [dbo].[AppointmentHistory] ADD CONSTRAINT [PK_AppointmentHistory] PRIMARY KEY CLUSTERED  ([Guid]) ON [PRIMARY]
 GO
