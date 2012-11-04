87076ebeba42825d3f27171422c3483ffc3d771d
votienphat <votienphat@gmail.com>
2012-11-04 08:05:43 +0000

    Them view Country

--- Database/SVN/Data/dbo.Room_Data.sql
+++ Database/SVN/Data/dbo.Room_Data.sql
@@ -1,7 +1,11 @@
 SET IDENTITY_INSERT [dbo].[Room] ON
-INSERT INTO [dbo].[Room] ([Id], [Title], [Note], [ServicesId], [IsDisabled], [CreateUser], [CreateDate], [UpdateUser], [UpdateDate]) VALUES (3, N'Office', NULL, 8, 1, N'GOWU', '2012-08-28 22:26:23.540', N'GOWU', '2012-08-28 23:04:13.327')
+INSERT INTO [dbo].[Room] ([Id], [Title], [Note], [ServicesId], [IsDisabled], [CreateUser], [CreateDate], [UpdateUser], [UpdateDate]) VALUES (6, N'ENT 2', NULL, 8, 0, N'GOWU', '2012-10-07 22:47:37.697', N'GOWU', '2012-10-07 23:16:49.690')
+INSERT INTO [dbo].[Room] ([Id], [Title], [Note], [ServicesId], [IsDisabled], [CreateUser], [CreateDate], [UpdateUser], [UpdateDate]) VALUES (7, N'ENT 3', NULL, 8, 0, N'GOWU', '2012-10-07 22:47:48.813', N'GOWU', '2012-10-07 23:02:49.330')
+SET IDENTITY_INSERT [dbo].[Room] OFF
+SET IDENTITY_INSERT [dbo].[Room] ON
 INSERT INTO [dbo].[Room] ([Id], [Title], [Note], [ServicesId], [IsDisabled], [CreateUser], [CreateDate], [UpdateUser], [UpdateDate]) VALUES (5, N'ENT 1', NULL, 8, 0, N'GOWU', '2012-08-16 20:59:25.873', N'GOWU', '2012-08-28 23:00:45.367')
 SET IDENTITY_INSERT [dbo].[Room] OFF
 SET IDENTITY_INSERT [dbo].[Room] ON
-INSERT INTO [dbo].[Room] ([Id], [Title], [Note], [ServicesId], [IsDisabled], [CreateUser], [CreateDate], [UpdateUser], [UpdateDate]) VALUES (4, N'Xray 1', NULL, 9, 0, N'GOWU', '2012-09-12 23:19:14.967', N'GOWU', '2012-09-12 23:19:14.967')
+INSERT INTO [dbo].[Room] ([Id], [Title], [Note], [ServicesId], [IsDisabled], [CreateUser], [CreateDate], [UpdateUser], [UpdateDate]) VALUES (3, N'Office', NULL, 8, 0, N'GOWU', '2012-08-28 22:26:23.540', N'GOWU', '2012-10-08 19:55:46.510')
+INSERT INTO [dbo].[Room] ([Id], [Title], [Note], [ServicesId], [IsDisabled], [CreateUser], [CreateDate], [UpdateUser], [UpdateDate]) VALUES (4, N'Xray 1', NULL, 9, 0, N'GOWU', '2012-09-12 23:19:14.967', N'GOWU', '2012-10-07 23:08:12.907')
 SET IDENTITY_INSERT [dbo].[Room] OFF
