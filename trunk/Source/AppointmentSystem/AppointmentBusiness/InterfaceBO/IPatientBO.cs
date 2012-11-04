5764e2cbfca93a5019bb1e122bcee5b9c5aa902e
votienphat@gmail.com <votienphat@gmail.com@81d2c78d-55ee-6d77-9ada-d03d0910548c>
2012-11-04 15:47:13 +0000

    Them xong chuc nang create/update patient trong trang appt

---
+++ trunk/Source/AppointmentSystem/AppointmentBusiness/InterfaceBO/IPatientBO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AppointmentSystem.Entities;
+
+namespace AppointmentBusiness.BO
+{
+    public interface IPatientBO
+    {
+        /// <summary>
+        /// Insert mot appointment
+        /// </summary>
+        /// <param name="patient"></param>
+        /// <param name="message"></param>
+        bool Insert(VcsPatient patient, ref string message);
+
+        /// <summary>
+        /// Update mot appointment
+        /// </summary>
+        /// <param name="patient"></param>
+        /// <param name="message"></param>
+        bool Update(ref VcsPatient patient, ref string message);
+    }
+}
