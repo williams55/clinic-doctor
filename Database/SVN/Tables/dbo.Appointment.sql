9221780d9447a072c4e64585984830abe23822cd
votienphat <votienphat@gmail.com>
2012-11-07 08:13:56 +0000

    Them trigger luu history khi change status cua Appt

--- Database/SVN/Tables/dbo.Appointment.sql
+++ Database/SVN/Tables/dbo.Appointment.sql
@@ -18,6 +18,59 @@ CREATE TABLE [dbo].[Appointment]
 [UpdateUser] [nvarchar] (200) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [UpdateDate] [datetime] NOT NULL CONSTRAINT [DF_Appointment_UpdateDate] DEFAULT (getdate())
 ) ON [PRIMARY]
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE TRIGGER [dbo].[trgInsertHistory]
+   ON  [dbo].[Appointment]
+   AFTER UPDATE
+AS 
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+	
+	declare @OldStatus varchar(20)
+	declare @NewStatus varchar(20)
+	DECLARE @AppointmentId nvarchar(20)
+	DECLARE @UpdateUser nvarchar(200)
+	
+	select @AppointmentId=i.Id
+			,@NewStatus = i.StatusId
+			,@UpdateUser = i.UpdateUser
+	from inserted i;
+	
+	select @OldStatus=d.StatusId
+	from deleted d
+	WHERE d.Id = @AppointmentId
+	
+	IF @OldStatus <> @NewStatus
+	BEGIN
+		INSERT INTO dbo.AppointmentHistory
+		        ( Guid ,
+		          AppointmentId ,
+		          Note ,
+		          CreateUser ,
+		          CreateDate
+		        )
+		VALUES  ( NEWID() , -- Guid - uniqueidentifier
+		          @AppointmentId , -- AppointmentId - nvarchar(20)
+		          N'User ' + @UpdateUser + ' change appointment ' + @AppointmentId + ' from status ' + @OldStatus + ' to ' + @NewStatus , -- Note - nvarchar(500)
+		          @UpdateUser , -- CreateUser - nvarchar(200)
+		          GETDATE()  -- CreateDate - datetime
+		        )
+	END
+
+END
+GO
+
 ALTER TABLE [dbo].[Appointment] ADD
 CONSTRAINT [FK_Appointment_Roster] FOREIGN KEY ([RosterId]) REFERENCES [dbo].[Roster] ([Id])
 ALTER TABLE [dbo].[Appointment] ADD
