a579a7656fbff6c929b6c0967c364efccd61f60a
votienphat <votienphat@gmail.com>
2012-11-07 09:14:25 +0000

    Update data

--- Database/SVN/Data/dbo.RosterType_Data.sql
+++ Database/SVN/Data/dbo.RosterType_Data.sql
@@ -1,7 +1,7 @@
 SET IDENTITY_INSERT [dbo].[RosterType] ON
-INSERT INTO [dbo].[RosterType] ([Id], [Title], [IsBooked], [ColorCode], [Note], [IsDisabled], [CreateUser], [CreateDate], [UpdateUser], [UpdateDate]) VALUES (3, N'Break', 0, NULL, NULL, 0, N'GOWU', '2012-09-07 11:44:37.467', N'GOWU', '2012-09-07 11:44:37.467')
-SET IDENTITY_INSERT [dbo].[RosterType] OFF
-SET IDENTITY_INSERT [dbo].[RosterType] ON
 INSERT INTO [dbo].[RosterType] ([Id], [Title], [IsBooked], [ColorCode], [Note], [IsDisabled], [CreateUser], [CreateDate], [UpdateUser], [UpdateDate]) VALUES (1, N'GP', 1, '#ffaad4', NULL, 0, N'GOWU', '2012-07-08 16:43:35.310', N'GOWU', '2012-07-08 16:43:35.310')
 INSERT INTO [dbo].[RosterType] ([Id], [Title], [IsBooked], [ColorCode], [Note], [IsDisabled], [CreateUser], [CreateDate], [UpdateUser], [UpdateDate]) VALUES (2, N'Meeting', 0, '#FFF000', NULL, 0, N'GOWU', '2012-07-08 16:43:42.680', N'GOWU', '2012-07-08 16:43:42.680')
+SET IDENTITY_INSERT [dbo].[RosterType] OFF
+SET IDENTITY_INSERT [dbo].[RosterType] ON
+INSERT INTO [dbo].[RosterType] ([Id], [Title], [IsBooked], [ColorCode], [Note], [IsDisabled], [CreateUser], [CreateDate], [UpdateUser], [UpdateDate]) VALUES (3, N'Break', 0, '#003366', NULL, 0, N'GOWU', '2012-09-07 11:44:37.467', N'GOWU', '2012-09-07 11:44:37.467')
 SET IDENTITY_INSERT [dbo].[RosterType] OFF
