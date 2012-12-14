1e39e6ff03c13e1617153eb57e59a64af53412e6
quangvy <quangvy@gmail.com>
2012-12-14 04:56:50 +0000

    Fix Role Provider

--- Source/AppointmentSystem/ApptSite/Logout.aspx.cs
+++ Source/AppointmentSystem/ApptSite/Logout.aspx.cs
@@ -5,12 +5,14 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using ApptSite;
+using System.Web.Security;
 
 public partial class Logout : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
         AccountSession.Session = null;
-        Response.Redirect(ResolveUrl("~/Login.aspx"));
+        //Response.Redirect(ResolveUrl("~/Login.aspx"));
+        FormsAuthentication.RedirectToLoginPage();
     }
 }
