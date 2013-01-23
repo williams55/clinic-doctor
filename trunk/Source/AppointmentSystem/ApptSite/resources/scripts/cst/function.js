81473af4f61b1344628979c9a8dae3a02473f7a0
votienphat@gmail.com <votienphat@gmail.com@81d2c78d-55ee-6d77-9ada-d03d0910548c>
2013-01-23 14:53:13 +0000

    1. Remove redundant files 2. Sort doctor in appointment page

--- trunk/Source/AppointmentSystem/ApptSite/resources/scripts/cst/function.js
+++ trunk/Source/AppointmentSystem/ApptSite/resources/scripts/cst/function.js
@@ -1,12 +1,1 @@
-﻿function isEmail(string) {
-    if (string.search(/^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+$/) != -1) {
-        return true;
-    }
-    return false; 
-}
-function isPhone(string) {
-    if (string.search(/\d{3}\-\d{3}\-\d{4}/)) {
-        return true;
-    }
-    return false;
-}
+﻿
