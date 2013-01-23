a70fcca06a64b8e130301ba5faaf75575964e265
votienphat <votienphat@gmail.com>
2013-01-23 16:49:23 +0000

    Add data for Message

--- Database/SVN/Data/dbo.MessageConfig_Data.sql
+++ Database/SVN/Data/dbo.MessageConfig_Data.sql
@@ -1,3 +1,7 @@
+INSERT INTO [dbo].[MessageConfig] ([MessageKey], [MessageValue], [Description]) VALUES ('ATH0002', N'You have no right to read', NULL)
+INSERT INTO [dbo].[MessageConfig] ([MessageKey], [MessageValue], [Description]) VALUES ('ATH0003', N'You have no right to create', NULL)
+INSERT INTO [dbo].[MessageConfig] ([MessageKey], [MessageValue], [Description]) VALUES ('ATH0004', N'You have no right to update', NULL)
+INSERT INTO [dbo].[MessageConfig] ([MessageKey], [MessageValue], [Description]) VALUES ('ATH0005', N'You have no right to delete', NULL)
 INSERT INTO [dbo].[MessageConfig] ([MessageKey], [MessageValue], [Description]) VALUES ('ATH0001', N'You''re still not login or your session was expired', NULL)
 INSERT INTO [dbo].[MessageConfig] ([MessageKey], [MessageValue], [Description]) VALUES ('GEN0001', N'System error. Please contact Administrator', NULL)
 INSERT INTO [dbo].[MessageConfig] ([MessageKey], [MessageValue], [Description]) VALUES ('STR0001', N'{0}''s length is over {1}', NULL)
