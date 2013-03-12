4a4c4c7d903b2fcb02499855d863c43abc35e1ac
votienphat@gmail.com <votienphat@gmail.com@81d2c78d-55ee-6d77-9ada-d03d0910548c>
2013-03-12 16:21:39 +0000

    [DB] Unlink data 1 số bảng, Thêm Tooltip vào title của các row trong bảng Status

--- trunk/Database/SVN/Data/dbo.AppointmentGroup_Data.sql
+++
@@ -1,4 +0,0 @@
-SET IDENTITY_INSERT [dbo].[AppointmentGroup] ON
-INSERT INTO [dbo].[AppointmentGroup] ([Id], [Title], [Note], [PriorityIndex], [IsDisabled], [CreateUser], [CreateDate], [UpdateUser], [UpdateDate], [UnitId]) VALUES (1, N'First Group', N'First Group', 1, 0, NULL, '2012-07-08 11:41:13.873', NULL, '2012-07-08 11:41:13.873', 1)
-INSERT INTO [dbo].[AppointmentGroup] ([Id], [Title], [Note], [PriorityIndex], [IsDisabled], [CreateUser], [CreateDate], [UpdateUser], [UpdateDate], [UnitId]) VALUES (2, N'X-Ray', N'X-Ray', 1, 0, N'GOWU', '2012-07-08 11:52:16.027', N'GOWU', '2012-07-08 11:52:16.027', 2)
-SET IDENTITY_INSERT [dbo].[AppointmentGroup] OFF
--- trunk/Database/SVN/Data/dbo.Room_Data.sql
+++
@@ -1,7 +0,0 @@
-SET IDENTITY_INSERT [dbo].[Room] ON
-INSERT INTO [dbo].[Room] ([Id], [Title], [Note], [ServicesId], [IsDisabled], [CreateUser], [CreateDate], [UpdateUser], [UpdateDate]) VALUES (3, N'CS1', NULL, 7, 0, N'duong.phan', '2012-11-09 10:46:46.310', N'duong.phan', '2012-11-09 10:46:46.310')
-INSERT INTO [dbo].[Room] ([Id], [Title], [Note], [ServicesId], [IsDisabled], [CreateUser], [CreateDate], [UpdateUser], [UpdateDate]) VALUES (4, N'CS2', NULL, 7, 0, N'duong.phan', '2012-11-09 10:48:04.623', N'duong.phan', '2012-11-09 10:48:04.623')
-INSERT INTO [dbo].[Room] ([Id], [Title], [Note], [ServicesId], [IsDisabled], [CreateUser], [CreateDate], [UpdateUser], [UpdateDate]) VALUES (5, N'CS3', NULL, 7, 0, N'duong.phan', '2012-11-09 10:48:31.793', N'duong.phan', '2012-11-09 10:48:31.793')
-INSERT INTO [dbo].[Room] ([Id], [Title], [Note], [ServicesId], [IsDisabled], [CreateUser], [CreateDate], [UpdateUser], [UpdateDate]) VALUES (6, N'CS4', NULL, 7, 0, N'duong.phan', '2012-11-09 10:48:45.403', N'duong.phan', '2012-11-09 10:48:45.403')
-INSERT INTO [dbo].[Room] ([Id], [Title], [Note], [ServicesId], [IsDisabled], [CreateUser], [CreateDate], [UpdateUser], [UpdateDate]) VALUES (7, N'CS5', NULL, 7, 0, N'duong.phan', '2012-11-09 10:48:54.653', N'duong.phan', '2012-11-09 10:48:54.653')
-SET IDENTITY_INSERT [dbo].[Room] OFF
